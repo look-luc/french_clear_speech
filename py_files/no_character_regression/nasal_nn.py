39708a7292780f5693980f4265b116c084d92600
lucdenardi <denardiluc@gmail.com>
2025-12-08 20:28:48 -0700

    tweaking

--- py_files/no_character_regression/nasal_nn.py
+++ py_files/no_character_regression/nasal_nn.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         # Configuration
         epoch_range = 800
         batches = 256
-        hidden_layer = 512
+        hidden_layer = 256
         parameters = [epoch_range, batches, hidden_layer]
 
         print(f"Epochs: {parameters[0]} Batch: {parameters[1]} Hidden: {parameters[2]}")
