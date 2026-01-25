87e1c2f7e2f96c60ea4480eba53353a6255b3f83
lucdenardi <denardiluc@gmail.com>
2026-01-25 13:19:12 -0700

    finished making regressional random forest

--- py_files/decision tree/main.py
+++ py_files/decision tree/main.py
@@ -1,15 +1,28 @@
 import warnings
-
+import rand_forest_regression
+import rand_forest_bi
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 warnings.filterwarnings('ignore')
 
 def main():
     df = pd.read_csv("../../data/vowel_data_all_LabPhon.csv")  # getting the data
-    df = df.dropna(subset=['Target'])
+
     # starting to work on random forest
     x = df.drop(columns=['vowelSAMPA', 'Target'])
     y = df['Target']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    regression_model = rand_forest_regression.RandomForest_nasal(
+        x_train=X_train,
+        x_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        num_estimators=100
+    )
+
+    print(regression_model.prediction())
     return 0
 
 
