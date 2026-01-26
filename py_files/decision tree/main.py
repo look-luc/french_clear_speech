a70dcfeb8fbfcb55da1539c565f0cf86a6597529
lucdenardi <denardiluc@gmail.com>
2026-01-26 10:00:17 -0700

    worked on showing tree func + get data for multiple data

--- py_files/decision tree/main.py
+++ py_files/decision tree/main.py
@@ -40,11 +40,9 @@ def main():
         ]
     )
 
-    y = df[
-        'Target Task',
-        'Target Clarity',
-        'Target Nasality'
-    ]
+    y = df[['Target Task','Target Clarity','Target Nasality']]
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     return 0
 
 
--- py_files/decision tree/show_tree.py
+++ py_files/decision tree/show_tree.py
@@ -2,19 +2,19 @@ import pandas as pd
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 
-def show_tree(model, title: str, filename: str, df: pd.DataFrame):
-    model_estimator = model.estimators_[0]
-
-    plt.figure(figsize=(20, 10))
-    plt.title(title)
-    plot_tree(
-        model_estimator,
-        feature_names= df.columns.tolist(),
-        show_leaf_values=True,
-        show_shapes=True,
-        show_edges=True,
-        filled=True,
-        rounded=True,
-        fontsize=10,
-    )
-    plt.savefig(f'{filename}.png')
+def show_tree(model, title: str, filename: str, x: pd.DataFrame, y: pd.DataFrame, num_features: int):
+    for i in range(num_features):
+        plt.figure(figsize=(20, 10))
+        plt.title(title)
+        plot_tree(
+            model.estimators_[i],
+            feature_names= x.columns.tolist(),
+            class_names= y.columns.tolist(),
+            show_leaf_values=True,
+            show_shapes=True,
+            show_edges=True,
+            filled=True,
+            rounded=True,
+            fontsize=10,
+        )
+        plt.savefig(f'{filename}_{i}.png')
