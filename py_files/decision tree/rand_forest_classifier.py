7b1cca1103d64022c91ea073c35e7cdd94f3b7ae
lucdenardi <denardiluc@gmail.com>
2026-02-10 09:41:17 -0700

    starting new type of rand forest

---
+++ py_files/decision tree/rand_forest_classifier.py
@@ -0,0 +1,13 @@
+import warnings
+import rand_forest_regression
+import rand_forest_bi
+import pandas as pd
+from sklearn.model_selection import train_test_split
+warnings.filterwarnings('ignore')
+
+def main():
+    df = pd.read_csv("../../data/ML Copy - FRENCH RESULTS - 2025 - Matching Datasets (VLOOKUP).csv")
+    return 0
+
+if __name__ == '__main__':
+    main()
