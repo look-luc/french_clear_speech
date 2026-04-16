702c357a06826e17fec8e6f75e6b92265510a09e
lucdenardi <denardiluc@gmail.com>
2026-04-16 12:42:23 -0600

    working on experiments

--- py_files/decision_tree/experiment_2.py
+++ py_files/decision_tree/experiment_2.py
@@ -8,18 +8,20 @@ def main():
     full_data = pd.read_csv("../../data/tache_lecture.csv")
     full_data = full_data[full_data["timepoint"] == 3]
 
-    nasal_appendix = full_data[
-        full_data["appendix"] == 'n' or full_data["appendix"] == 'n+c'
-    ].drop([
-        'timepoint', 'appendix', 'appendix_dur'
-    ], axis=1)
+    nasal_appendix = full_data.drop(
+        ['timepoint', 'appendix', 'appendix_dur', 'creak_app'],
+        axis=1
+    )
 
-    creak_appendix = full_data[
-        full_data["appendix"] == 'c' or full_data["appendix"] == 'n+c'
-        ].drop([
-        'timepoint', 'appendix', 'appendix_dur'
-    ], axis=1)
+    creak_appendix = full_data.drop(
+        ['timepoint', 'appendix', 'appendix_dur', 'nasal_app'],
+        axis=1
+    )
 
+    nasal_appendix_dur = full_data.drop(
+        ['timepoint', 'appendix', 'nasal_app', 'creak_app'],
+        axis=1
+    )
 
     return 0
 
