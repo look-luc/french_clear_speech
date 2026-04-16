fa74d9704376552dacc2b47b23a755bd93045116
lucdenardi <denardiluc@gmail.com>
2026-04-16 13:29:05 -0600

    working on experiments

--- py_files/decision_tree/experiment_2.py
+++ py_files/decision_tree/experiment_2.py
@@ -1,27 +1,72 @@
 import warnings
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import rand_forest_regression
+from show_tree import importance_trees, permutation
 
 warnings.filterwarnings('ignore')
 
 def main():
-    full_data = pd.read_csv("../../data/tache_lecture.csv")
+    full_data = pd.read_csv("../../data/tache_lecture.csv").drop("clarity", axis=1)
     full_data = full_data[full_data["timepoint"] == 3]
 
     nasal_appendix = full_data.drop(
-        ['timepoint', 'appendix', 'appendix_dur', 'creak_app'],
+        ['timepoint', 'appendix', 'appendix_dur', 'nasal_app', 'creak_app', 'clarity', 'target'],
         axis=1
     )
+    nasal_target = full_data['nasal_app']
 
     creak_appendix = full_data.drop(
-        ['timepoint', 'appendix', 'appendix_dur', 'nasal_app'],
+        ['timepoint', 'appendix', 'appendix_dur', 'nasal_app', 'creak_app', 'clarity', 'target'],
         axis=1
     )
+    creak_target = full_data['creak_app']
 
-    nasal_appendix_dur = full_data.drop(
-        ['timepoint', 'appendix', 'nasal_app', 'creak_app'],
-        axis=1
-    )
+    data = [nasal_appendix, creak_appendix]
+    targets = [nasal_target, creak_target]
+    exp = ["nasal_only", "creak_only"]
+
+    for x, y, experiment in zip(data, targets, exp):
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        regression_model = rand_forest_regression.RandomForest_nasal(
+            x_train=X_train,
+            x_test=X_test,
+            y_train=y_train,
+            y_test=y_test,
+            num_estimators=1000,
+            depth=30
+        )
+
+        regression_model.modeling()
+        accuracy, report, oob = regression_model.prediction()
+
+        print(f"Validation Accuracy: {accuracy:.4f}")
+        print(f"OOB Score:           {oob:.4f}")
+        print(f"\nClassification Report:\n{report}")
+
+        # Feature Importance Visualizations
+        importance_trees(
+            regression_model,
+            "MDI Feature Importance",
+            f"importance_{experiment}",
+            "features",
+            "Mean decrease in impurity",
+            x
+        )
+
+        permutation(
+            regression_model.model,
+            "Importance features via permutation on full model",
+            f"permutation_{experiment}",
+            "features",
+            "Mean accuracy decrease",
+            x,
+            X_test,
+            y_test,
+        )
 
     return 0
 
