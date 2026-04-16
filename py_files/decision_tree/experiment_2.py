7a3030a2d68138e6f50742713dbb30fb39bfc288
lucdenardi <denardiluc@gmail.com>
2026-04-16 12:29:00 -0600

    working on experiments

--- py_files/decision_tree/experiment_2.py
+++ py_files/decision_tree/experiment_2.py
@@ -1,4 +1,16 @@
+import warnings
+
+import pandas as pd
+
+warnings.filterwarnings('ignore')
+
 def main():
+    full_data = pd.read_csv("../../data/tache_lecture.csv")
+    full_data = full_data[full_data["timepoint"] == 3]
+
+    nasal_appendix = full_data[
+        full_data["appendix"] == 'n' or full_data["appendix"] == 'n+c'
+        ].drop('timepoint', axis=1)
     return 0
 
 
